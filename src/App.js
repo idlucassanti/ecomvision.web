3b32dfba71f93aae21c8bfe3e40f1ffdefc277b1
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2022-12-12 19:11:10 -0300

    react router dom with Layout default all routes and redirect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { themeSettings } from "theme";
 
 function App() {
@@ -10,9 +11,17 @@ function App() {
 
   return ( 
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>    
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
